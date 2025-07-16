82b37ac7b80cccb74df3ea309513d5c8176d37b6
kevinmarlis <kevinmarlis@gmail.com>
2025-07-16 22:53:23 +0000

    Better organization of maap submit job calls and added initial support for NOAA FTP requests

--- gmsec_service/handlers/directive_handler.py
+++ gmsec_service/handlers/directive_handler.py
@@ -2,6 +2,7 @@ import json
 import os
 import libgmsec_python3 as lp
 from time import sleep
+from typing import Optional
 
 from maap.maap import MAAP, DPSJob
 
@@ -50,7 +51,7 @@ class GmsecJobStatus(GmsecRequestHandler):
     def __init__(self, directive_keyword, directive_string):
         super().__init__(directive_keyword, directive_string)
 
-    def get_job_id(self) -> str:
+    def get_concept_id(self) -> str:
         """ """
         job_id = self.directive_string_data.get("job-id", "N/A")
         if job_id == "N/A":
@@ -103,7 +104,7 @@ class GmsecSubmitJob(GmsecRequestHandler):
     def get_ingest_concept_id(self) -> str:
         concept_id = self.directive_string_data.get("concept_id")
         if not concept_id:
-            raise ValueError("Unable to extract 'products' from DIRECTIVE-STRING.")
+            raise ValueError("Unable to extract 'concept_id' from DIRECTIVE-STRING.")
         return concept_id
     
     def get_ingest_product_path(self) -> str:
@@ -115,17 +116,13 @@ class GmsecSubmitJob(GmsecRequestHandler):
         return product_path[0]
     
     def get_ingest_product_type(self) ->str:
-        # TODO: directive string will contain a "format" field which will dictate
-        # which ingest workflow to use
         product_format = self.directive_string_data.get("format")
         if not product_format:
             raise ValueError("Unable to extract 'format' from DIRECTIVE-STRING.")
         return product_format
 
-    def get_ingest_variables(self):
+    def get_ingest_variables(self) -> Optional[str]:
         product_variables = self.directive_string_data.get("essential_variables")
-        if not product_variables:
-            raise ValueError("Unable to extract 'format' from DIRECTIVE-STRING.")
         return product_variables
 
     def trigger_ingest(self) -> JobState:
@@ -136,56 +133,60 @@ class GmsecSubmitJob(GmsecRequestHandler):
         concept_id = self.get_ingest_concept_id()
         product_path = self.get_ingest_product_path()
         product_type = self.get_ingest_product_type()
+        ingest_variables = self.get_ingest_variables()
         
-        if product_type == "netcdf":
-            variables = self.get_ingest_variables()
-            try:    
-                job: DPSJob = maap.submitJob(
-                    identifier="gmsec_ingest",
-                    algo_id="czdt-iss-product-ingest",
-                    version="auto-create-collection",
-                    queue="maap-dps-czdt-worker-8gb",
-                    input_s3=product_path,
-                    collection_id=concept_id,
-                    variables="FloodedFrac_tavg",
-                    s3_bucket="czdt-hysds-dataset",
-                    s3_prefix="ingest",
-                    role_arn="arn:aws:iam::011528287727:role/czdt-hysds-verdi-role",
-                    cmss_logger_host="http://44.242.188.25:8000",
-                    mmgis_host="http://35.86.216.193:8888",
-                    titiler_token_secret_name="czdt_titiler_token",
-                    job_queue="maap-dps-czdt-worker-8gb",
-                    zarr_config_url="s3://maap-ops-workspace/rileykk/sample_lis_cfg.yaml",
-                    maap_host="api.maap-project.org"
-                )
-            except Exception as e:
-                lp.log_error(f"Unable to submit job {e}")
-                return JobState.from_maap_status("failed", "N/A")
-        elif product_type == "zarr":
-            try:
-                job: DPSJob = maap.submitJob(
-                    identifier="gmsec_ingest",
-                    algo_id="czdt-iss-product-ingest",
-                    version="auto-create-collection",
-                    queue="maap-dps-czdt-worker-8gb",
-                    input_s3=product_path,
-                    collection_id=concept_id,
-                    s3_bucket="czdt-hysds-dataset",
-                    s3_prefix="ingest",
-                    role_arn="arn:aws:iam::011528287727:role/czdt-hysds-verdi-role",
-                    cmss_logger_host="http://44.242.188.25:8000",
-                    mmgis_host="http://35.86.216.193:8888",
-                    titiler_token_secret_name="czdt_titiler_token",
-                    job_queue="maap-dps-czdt-worker-8gb",
-                    zarr_config_url="s3://maap-ops-workspace/rileykk/sample_lis_cfg.yaml",
-                    maap_host="api.maap-project.org"
-                )
-            except Exception as e:
-                lp.log_error(f"Unable to submit job {e}")
-                return JobState.from_maap_status("failed", "N/A")    
-        elif product_type == "gpkg":
+        # gpkg currently not supported
+        if product_type == "gpkg":
             lp.log_error(f"Unable to submit gpkg job")
             return JobState.from_maap_status("failed", "N/A")
+        
+        # Trigger ingest NOAA FTP
+        if any(["noaa" in v.lower() for v in string_data.values()]):
+            job_args = {
+                identifier: "ftp-ingest-021",
+                algo_id: "czdt-ftp-ingest",
+                version: "auto-create-collection",
+                queue: "maap-dps-czdt-worker-8gb",
+                ftp_server: "floodlight.ssec.wisc.edu",
+                area_of_interest: "021", # Chesapeake Bay area
+                s3_bucket: "czdt-hysds-dataset",
+                s3_prefix: "ingest",
+                role_arn: "arn:aws:iam::011528287727:role/czdt-hysds-verdi-role",
+                cmss_logger_host: "http://44.242.188.25:8000",
+                mmgis_host: "http://35.86.216.193:8888",
+                titiler_token_secret_name: "czdt_titiler_token",
+                overwrite_existing: "false"
+            }
+            
+        # Trigger normal product ingest
+        else:
+            job_args = {
+                identifier: "gmsec_ingest",
+                algo_id: "czdt-iss-product-ingest",
+                version: "auto-create-collection",
+                queue: "maap-dps-czdt-worker-8gb",
+                input_s3: product_path,
+                collection_id: concept_id,
+                s3_bucket: "czdt-hysds-dataset",
+                s3_prefix: "ingest",
+                role_arn: "arn:aws:iam::011528287727:role/czdt-hysds-verdi-role",
+                cmss_logger_host: "http://44.242.188.25:8000",
+                mmgis_host: "http://35.86.216.193:8888",
+                titiler_token_secret_name: "czdt_titiler_token",
+                job_queue: "maap-dps-czdt-worker-8gb",
+                zarr_config_url: "s3://maap-ops-workspace/rileykk/sample_lis_cfg.yaml",
+                maap_host: "api.maap-project.org"
+            }
+        
+            if ingest_variables:
+                job_args["variables"] = ingest_variables
+        
+        try:
+            job: DPSJob = maap.submitJob(**job_args)
+        except Exception as e:
+            lp.log_error(f"Unable to submit job {e}")
+            return JobState.from_maap_status("failed", "N/A") 
+        
         if job.status == "success":
             return JobState.from_maap_status("accepted", job.id)
         return JobState.from_maap_status(job.status, job.id)
